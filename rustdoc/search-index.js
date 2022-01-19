a48fbb2c547eea4adecfbbc7307cc6589c68775f
sunshowers <rain@sunshowers.io>
2022-01-19 22:09:56 +0000

    Deploying to gh-pages from @ sunshowers-code/enable-ansi-support@4504bcffd65a9069f76de88fe144a41e00ae347c 🚀

--- rustdoc/search-index.js
+++ rustdoc/search-index.js
@@ -1,4 +1,4 @@
 var searchIndex = JSON.parse('{\
-"enable_ansi_support":{"doc":"Enable ANSI code support on Windows 10 and above.","t":[5],"n":["enable_ansi_support"],"q":["enable_ansi_support"],"d":["Enables ANSI code support on Windows 10."],"i":[0],"f":[[[],[["result",4,["u32"]],["u32",15]]]],"p":[]}\
+"enable_ansi_support":{"doc":"Enable ANSI code support on Windows 10 and above.","t":[5],"n":["enable_ansi_support"],"q":["enable_ansi_support"],"d":["Enables ANSI code support on Windows 10."],"i":[0],"f":[[[],["result",4,[["u32",15]]]]],"p":[]}\
 }');
 if (window.initSearch) {window.initSearch(searchIndex)};
