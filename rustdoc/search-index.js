bebae41dcfe4b57010c05c79c835bc5fa73ecb2d
sunshowers <rain@sunshowers.io>
2025-10-02 18:54:30 +0000

    Deploying to gh-pages from @ sunshowers-code/enable-ansi-support@9c9c0e25aff727f7bbacb81a8872ea4eccb1cb11 🚀

---
+++ rustdoc/search-index.js
@@ -0,0 +1,4 @@
+var searchIndex = new Map(JSON.parse('[["enable_ansi_support",{"t":"H","n":["enable_ansi_support"],"q":[[0,"enable_ansi_support"],[1,"std::io::error"],[2,"core::result"]],"i":"`","f":"{{}{{f{bd}}}}","D":"d","p":[[1,"unit"],[5,"Error",1],[6,"Result",2,null,1]],"r":[],"b":[],"c":"OjAAAAAAAAA=","e":"OjAAAAAAAAA=","P":[]}]]'));
+if (typeof exports !== 'undefined') exports.searchIndex = searchIndex;
+else if (window.initSearch) window.initSearch(searchIndex);
+//{"start":39,"fragment_lengths":[281]}
